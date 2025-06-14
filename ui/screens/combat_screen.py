1d96e0d9dfddfdc0d749b4c656e1be1a551a9277
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-06-14 09:34:28 -0300

    Início da implementação do combate

--- ui/screens/combat_screen.py
+++ ui/screens/combat_screen.py
@@ -1,10 +1,7 @@
-from data.repository import get_random_enemy
+import time
+from data.repository import get_random_enemy, load_data
 from ui.components import clear_terminal, display_status
-
-def display_monster():
-    enemy = get_random_enemy()
-    print(f"You found a {enemy["name"]}!")
-    
+from utils import throw_fate_dices   
 
 def display_combat_menu():
     print("Combat Menu:")
@@ -13,7 +10,46 @@ def display_combat_menu():
     return input()
 
 def combat_screen():
-    clear_terminal()
-    display_status()
-    display_monster()
-    display_combat_menu()
+    data = load_data()
+
+    enemy = get_random_enemy()
+    enemy_physical_stress = enemy["stress"]["physical"]
+    enemy_mental_stress = enemy["stress"]["mental"]
+
+    print(f"You found a {enemy["name"]}!")
+    time.sleep(1)
+
+    while True:
+        clear_terminal()
+        display_status()
+
+        print("[todo: display monster]")
+
+        answer = display_combat_menu()
+        match answer:
+            case "1":
+                print("Your roll:")
+                char_roll = throw_fate_dices()
+                time.sleep(1)
+
+                print("Added bonus:")
+                char_result = char_roll + data["skills"]["fight"]
+                print(f"{char_roll} + {data["skills"]["fight"]} = {char_result}")
+                time.sleep(1)
+
+                print("Enemy roll:")
+                enemy_roll = throw_fate_dices()
+                time.sleep(1)
+
+                print("Added bonus:")
+                enemy_result = enemy_roll + enemy["skills"]["fight"]
+                print(f"{enemy_roll} + {enemy["skills"]["fight"]} = {enemy_result}")
+                time.sleep(1)
+                
+            case "2":
+                break
+            case _:
+                print("Please select a valid option!")
+        
+        time.sleep(1)
+        
